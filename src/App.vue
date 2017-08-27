81d58844cce539571fbcb5f125cf0fbb26529e77
Sergio Dobri <sergiodobri@gmail.com>
2017-08-27 20:37:07 -0300

    hello word!!!

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    router-view
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="stylus">
+#app
+  margin-top 60px
+  color #2c3e50
+  text-align center
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+</style>
